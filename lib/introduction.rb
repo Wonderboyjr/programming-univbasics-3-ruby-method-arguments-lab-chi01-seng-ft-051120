eb1c162e88f7abc2cb9679909f90c3d733340e23
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-21 22:30:08 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,18 +1,13 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
-  end
+end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my is #{name} and I am learning to program in #{language}."
-end
-
-introduction("Jon")
-introduction_with_language("Jon", "Ruby")
-introduction_with_language_optional("Jon")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
@@ -83,22 +78,3 @@ end
 
 
 
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
